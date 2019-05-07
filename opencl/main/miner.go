fc5a28dfff3aa5f6d47b5bff7913245051279a25
yangjiePro <yangjie@jojoin.com>
2019-05-07 15:49:13 +0800

    gpu up

--- opencl/main/miner.go
+++ opencl/main/miner.go
@@ -7,10 +7,10 @@ import (
 	"fmt"
 	"github.com/xfong/go2opencl/cl"
 	"log"
-	"math/rand"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -28,12 +28,35 @@ type minerDeviceExecute struct {
 	blockHeight       uint32
 	blockCoinbaseMsg  [16]byte
 	blockCoinbaseAddr [21]byte
+
 	// 状态数据
 
 	// 挖矿状态
 	retry   bool // 重新尝试挖矿
 	success bool
 	nonce   []byte
+}
+
+// 执行任务
+type minerExecuteWork struct {
+	autoidx uint64
+
+	blockHeight       uint32
+	blkstuff          [89]byte
+	target            [32]byte
+	blockCoinbaseMsg  [16]byte
+	blockCoinbaseAddr [21]byte
+
+	// id标记
+	mid uint64
+	// 结果通知
+	resultCh chan *minerDeviceExecute
+	// 执行队列
+	executeQueueChList []chan *minerDeviceExecute
+	// 等待全部关闭
+	wg sync.WaitGroup
+	// 全部停止标记
+	stopMarkCh chan bool
 }
 
 type GpuMiner struct {
@@ -47,9 +70,13 @@ type GpuMiner struct {
 	//queues   []*cl.CommandQueue // 所有队列
 
 	// 执行队列
-	executeQueueChList []chan *minerDeviceExecute
+	//executeQueueChList []chan *minerDeviceExecute
 
-	///////////////////////////////
+	// 任务列表
+	minerExecuteWorkListCh chan *minerExecuteWork
+
+	// 执行任务
+	//////////////////
 
 	// device *cl.Device
 	// queue  *cl.CommandQueue
@@ -69,9 +96,11 @@ type GpuMiner struct {
 	printNumBase int
 	rebuild      bool // 强制重新编译
 
+	loopLock sync.Mutex
+
 	// msg
-	miningPrevId uint32
-	stopMarkCh   chan uint32
+	//miningPrevId uint32
+	//stopMarkCh   chan uint32
 }
 
 type MinerResult struct {
@@ -80,18 +109,197 @@ type MinerResult struct {
 	nonce   []byte
 }
 
+/*
 func (mr *GpuMiner) stopAll() {
 	if mr.miningPrevId > 0 {
 		// 清空待执行队列
 		mr.stopMarkCh <- mr.miningPrevId // 投喂线程
 
-		for range mr.executeQueueChList {
-			mr.stopMarkCh <- mr.miningPrevId // 执行线程
+		//for range mr.executeQueueChList {
+		//	mr.stopMarkCh <- mr.miningPrevId // 执行线程
+		//}
+	}
+}
+*/
+
+func (mr *GpuMiner) minerWorkLoop() {
+	for {
+		work := <-mr.minerExecuteWorkListCh
+		// 判断当前执行id
+		if work.mid < mr.autoidx {
+			continue // 永远只执行最后一个任务
 		}
+		// 执行
+		fmt.Printf("\n\ndo miner work<%d>, height<%d>, target<%s>, block<%s>\n", work.mid, work.blockHeight, hex.EncodeToString(work.target[:]), hex.EncodeToString(work.blkstuff[:]))
+		// 投喂执行单元
+		work.wg.Add(1)
+		go func(work *minerExecuteWork) {
+			defer func() {
+				fmt.Printf(" , work %d exit ", work.mid)
+				work.wg.Done()
+			}()
+			// 持续投喂
+			for i := 0; ; i++ {
+				// 判断停止
+				if work.mid < mr.autoidx {
+					return // 工作过期
+				}
+				// 已尝试全部
+				onmax := uint64(i)*uint64(mr.groupSize) > 4294967290
+				if onmax {
+					// 等待停止
+					fmt.Print(" , #retry reset coinbase to be new block stuff# ")
+					retry := &minerDeviceExecute{}
+					retry.retry = true
+					select {
+					case work.resultCh <- retry:
+					case <-time.After(time.Second * 3):
+					}
+					return
+				}
+				// 创建执行单例
+				deviceIndex := i % len(mr.devices)
+				device := mr.devices[deviceIndex]
+				//queue := mr.queues[deviceIndex]
+				work.autoidx += 1
+				chindex := (mr.exeWide * deviceIndex) + int(work.autoidx%uint64(mr.exeWide))
+				exe := &minerDeviceExecute{
+					work.autoidx,
+					uint32(deviceIndex),
+					device,
+					//mr.kernel,
+					//queue,
+					work.blkstuff,
+					work.target,
+					uint32(i) * uint32(mr.groupSize),
+					uint32(mr.groupSize),
+					work.blockHeight,
+					work.blockCoinbaseMsg,
+					work.blockCoinbaseAddr,
+					false,
+					false,
+					nil,
+				}
+				//fmt.Println("<<<<<<<<<<<<<<<< ")
+				select {
+				case <-time.After(time.Second * 3):
+					return // 三秒没有执行则退出
+				case work.executeQueueChList[chindex] <- exe:
+				}
+			}
+		}(work)
+		// 执行队列
+		// 执行挖矿
+		work.wg.Add(mr.executeSize)
+		for i := 0; i < mr.executeSize; i++ {
+			go func(work *minerExecuteWork, i int) {
+				defer func() {
+					fmt.Printf(" , execute %d-%d quit ", work.mid, i)
+					work.wg.Done()
+				}()
+				// 运行创建执行单元
+				iii1 := make([]byte, 32)
+				copy(iii1, work.target[:])
+				iii2 := make([]byte, 89)
+				copy(iii2, work.blkstuff[:])
+				// |cl.MemCopyHostPtr
+				input_target, _ := mr.context.CreateBuffer(cl.MemReadOnly|cl.MemCopyHostPtr, iii1)
+				//defer k
+				input_stuff, _ := mr.context.CreateBuffer(cl.MemReadOnly|cl.MemCopyHostPtr, iii2)
+				defer input_target.Release()
+				defer input_stuff.Release()
+
+				kernel, ke1 := mr.program.CreateKernel("miner_do_hash_x16rs_v1")
+				if ke1 != nil {
+					panic(ke1)
+				}
+				defer kernel.Release()
+
+				device := mr.devices[i/mr.exeWide]
+				queue, qe1 := mr.context.CreateCommandQueue(device, 0)
+				if qe1 != nil {
+					panic(qe1)
+				}
+				defer queue.Release()
+
+				fmt.Printf(" , execute %d-%d start ", work.mid, i)
+
+				for {
+					// 读取一个执行单例
+					// 判断停止
+					if work.mid < mr.autoidx {
+						return // 工作过期
+					}
+					select {
+					case exe := <-work.executeQueueChList[i]:
+						//fmt.Println("mr.executing(exe) >>>>>>>>>>>>>>>> ", exe.baseStart)
+						success := mr.executing(exe, kernel, queue, input_stuff, input_target)
+						if success {
+							// 成功返回
+							select {
+							case work.resultCh <- exe:
+							case <-time.After(time.Second * 3):
+							}
+							return
+						}
+					case <-time.After(time.Second * 3):
+						// 等待超时失败
+						return
+
+					}
+				}
+			}(work, i)
+		}
+		// 等待结束
+		go func(work *minerExecuteWork) {
+			work.wg.Wait()
+			fmt.Printf(" , finish close execute work %d. ", work.mid)
+		}(work)
 	}
 }
 
+//
 func (mr *GpuMiner) ReStartMiner(blockHeight uint32, blkstuff [89]byte, target [32]byte,
+	blockCoinbaseMsg [16]byte, blockCoinbaseAddr [21]byte,
+) *minerDeviceExecute {
+
+	mr.loopLock.Lock()
+
+	// 停止之前的工作，启动一个队列任务并等待
+	mr.autoidx += 1
+
+	executeQueueChList := make([]chan *minerDeviceExecute, mr.executeSize)
+	for i := 0; i < mr.executeSize; i++ {
+		executeQueueChList[i] = make(chan *minerDeviceExecute, mr.executeSize*8)
+	}
+
+	// 创建挖矿工作
+	minerWork := &minerExecuteWork{
+		0,
+		blockHeight,
+		blkstuff,
+		target,
+		blockCoinbaseMsg,
+		blockCoinbaseAddr,
+		mr.autoidx,
+		make(chan *minerDeviceExecute, 1),
+		executeQueueChList,
+		sync.WaitGroup{},
+		make(chan bool, mr.executeSize+1),
+	}
+
+	// 放入工作池
+	mr.minerExecuteWorkListCh <- minerWork
+
+	mr.loopLock.Unlock()
+
+	// 等待执行结果
+	resexe := <-minerWork.resultCh
+	return resexe
+}
+
+/*
+func (mr *GpuMiner) ReStartMiner_old(blockHeight uint32, blkstuff [89]byte, target [32]byte,
 	blockCoinbaseMsg [16]byte, blockCoinbaseAddr [21]byte,
 ) *minerDeviceExecute {
 	// 写入停止标记，重新开始下一轮挖矿
@@ -232,6 +440,9 @@ func (mr *GpuMiner) ReStartMiner(blockHeight uint32, blkstuff [89]byte, target [
 	//delete(mr.stopMark, mid)
 }
 
+
+*/
+
 // 执行
 func (mr *GpuMiner) executing(exe *minerDeviceExecute, kernel *cl.Kernel, queue *cl.CommandQueue, input_stuff *cl.MemObject, input_target *cl.MemObject) bool {
 
@@ -308,7 +519,8 @@ func (mr *GpuMiner) doGroupWork(exe *minerDeviceExecute, kernel *cl.Kernel, queu
 
 func (mr *GpuMiner) InitBuildProgram(openclPath string, platName string, dvid int, groupSize int, exeWide int, printNumBase int, rebuild bool) error {
 
-	mr.miningPrevId = 0
+	//mr.miningPrevId = 0
+	mr.autoidx = 0
 
 	mr.openclPath = openclPath
 	mr.platName = platName
@@ -317,6 +529,12 @@ func (mr *GpuMiner) InitBuildProgram(openclPath string, platName string, dvid in
 	mr.exeWide = exeWide
 	mr.printNumBase = printNumBase
 	mr.rebuild = rebuild
+
+	// 工作池
+	mr.minerExecuteWorkListCh = make(chan *minerExecuteWork, 16)
+
+	// 执行工作
+	go mr.minerWorkLoop()
 
 	var err error
 
@@ -358,13 +576,14 @@ func (mr *GpuMiner) InitBuildProgram(openclPath string, platName string, dvid in
 
 	// 队列大小
 	mr.executeSize = len(mr.devices) * mr.exeWide
-	mr.executeQueueChList = make([]chan *minerDeviceExecute, mr.executeSize)
-	for i := 0; i < mr.executeSize; i++ {
-		mr.executeQueueChList[i] = make(chan *minerDeviceExecute, mr.executeSize*8)
-	}
+
+	//mr.executeQueueChList = make([]chan *minerDeviceExecute, mr.executeSize)
+	//for i := 0; i < mr.executeSize; i++ {
+	//	mr.executeQueueChList[i] = make(chan *minerDeviceExecute, mr.executeSize*8)
+	//}
 
 	// 停止标记
-	mr.stopMarkCh = make(chan uint32) // , mr.executeSize + 1
+	//mr.stopMarkCh = make(chan uint32) // , mr.executeSize + 1
 
 	if mr.context, err = cl.CreateContext(mr.devices); err != nil {
 		panic(err)
